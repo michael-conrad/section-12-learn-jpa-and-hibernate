6b98fe867aba7e47060b2108ebd827ccbb658412
Michael Conrad <m.conrad.202@gmail.com>
2025-10-06 21:54:04 -0400

    Initialize Spring Boot project with JPA and Hibernate.

---
+++ src/test/java/com/michael_conrad/spring_boot/learn_jpa_and_hibernate/LearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael_conrad.spring_boot.learn_jpa_and_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
