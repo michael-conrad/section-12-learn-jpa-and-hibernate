b58a9378b54bb24d0161396ff7bc022c724bf9e4
Michael Conrad <m.conrad.202@gmail.com>
2025-10-13 00:01:57 -0400

    107. Step 10 - Getting Started with Spring Data JPA
    - Replaced `CourseJpaRepository` with `CourseSpringDataJpaRepository`.
    - Leveraged Spring Data JPA for CRUD operations in `CourseCommandLineRunner`.

--- src/main/java/com/michael_conrad/spring_boot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/michael_conrad/spring_boot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -1,6 +1,6 @@
 package com.michael_conrad.spring_boot.learn_jpa_and_hibernate.course;
 
-import com.michael_conrad.spring_boot.learn_jpa_and_hibernate.course.jdbc.CourseJpaRepository;
+import com.michael_conrad.spring_boot.learn_jpa_and_hibernate.course.spirngdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,18 +8,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
+    //    @Autowired
+//    private CourseJpaRepository repository;
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
         int id = 0;
-        repository.insert(new Course(++id, "Learn AWS Now!", "in28minutes"));
-        repository.insert(new Course(++id, "Learn Azure Now!", "in28minutes"));
-        repository.insert(new Course(++id, "Learn DevOps Now!", "in28minutes"));
-        repository.deleteById(1);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        repository.save(new Course(++id, "Learn AWS Now!", "in28minutes"));
+        repository.save(new Course(++id, "Learn Azure Now!", "in28minutes"));
+        repository.save(new Course(++id, "Learn DevOps Now!", "in28minutes"));
+        repository.deleteById(1L);
+
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
     }
 }
---
+++ src/main/java/com/michael_conrad/spring_boot/learn_jpa_and_hibernate/course/spirngdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,8 @@
+package com.michael_conrad.spring_boot.learn_jpa_and_hibernate.course.spirngdatajpa;
+
+import com.michael_conrad.spring_boot.learn_jpa_and_hibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+}
